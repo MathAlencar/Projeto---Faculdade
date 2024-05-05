ccdba07f20f942e1b9d43b6a592e6fc600e21026
CaioNoboa <caiosalvador@hotmail.com>
2024-05-05 11:38:48 -0300

    GET de produtos e funcionários

--- client/app.js
+++ client/app.js
@@ -8,22 +8,50 @@ dotenv.config({ path: './.env' })
 
 const app = express();
 
-const publicDirectory =  path.join(__dirname, './public');
-const publicDirectoryIMG =  path.join(__dirname, './img');
+const publicDirectory = path.join(__dirname, './public');
+const publicDirectoryIMG = path.join(__dirname, './img');
 
 app.use(express.static(publicDirectory));
 app.use(express.static(publicDirectoryIMG));
 app.set('viwe engine', 'hbs');
 app.use(cookieParser());
 
-app.use(express.urlencoded({extended: false})); // envia os dados para o seu server
+app.use(express.urlencoded({ extended: false })); // envia os dados para o seu server
 app.use(express.json()); // transforma os dados enviados em Json;
 
+app.use((request, response, next) => {
+    // Gambiarra para não ter problemas com 'CORS policy'
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PATCH,DELETE");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+})
+
 const rotaPages = require('./routes/pages');
-const rotaAuth= require('./routes/auth');
+const rotaAuth = require('./routes/auth');
+const rotaProdutos = require('./routes/produtos');
+const rotaFuncionarios = require('./routes/funcionarios');
 
 app.use('/', rotaPages);
 app.use('/auth', rotaAuth);
+app.use('/produtos', rotaProdutos);
+app.use('/funcionarios', rotaFuncionarios);
+
+// Tratamento de erro: quando não encontrar a rota:
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
@@ -39,7 +67,6 @@ db.connect( (err) => {
         console.log("Mysql connected...")
     }
 })
-
 
 app.listen(5000, () => {
     console.log("Server startd on 5000");
